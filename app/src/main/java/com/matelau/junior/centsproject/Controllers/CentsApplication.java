92877c48b5cd7778acd1e89a367434cfff88806c
Junior Matelau <matelau@gmail.com>
2014-11-22 06:56:02 -0700

    search activity view implemented - todo validate submission and api get

--- app/src/main/java/com/matelau/junior/centsproject/Controllers/CentsApplication.java
+++ app/src/main/java/com/matelau/junior/centsproject/Controllers/CentsApplication.java
@@ -1,7 +1,19 @@
 package com.matelau.junior.centsproject.Controllers;
 
+import android.app.Application;
+import android.content.Context;
+
 /**
  * Created by matelau on 11/20/14.
  */
-public class CentsApplication {
+public class CentsApplication extends Application{
+    static Context _centsContext;
+
+    public CentsApplication(){
+        _centsContext = this;
+    }
+
+    public static Context getAppContext() {return _centsContext;}
+
+
 }
--- app/src/main/java/com/matelau/junior/centsproject/Controllers/MainActivity.java
+++ app/src/main/java/com/matelau/junior/centsproject/Controllers/MainActivity.java
@@ -2,20 +2,66 @@ package com.matelau.junior.centsproject.Controllers;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.matelau.junior.centsproject.R;
 
 
 public class MainActivity extends Activity {
+//    Hex Color #884412
+    private EditText _editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_search);
+        ImageButton search_submit = (ImageButton) findViewById(R.id.search_button);
+        _editText = (EditText) findViewById(R.id.editText1);
+        _editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH ||
+                        actionId == EditorInfo.IME_ACTION_DONE ||
+                        event.getAction() == KeyEvent.ACTION_DOWN &&
+                                event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                        handleSubmit();
+                        return true;
+                }
+                    return false;
+            }
+        });
+        search_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleSubmit();
+            }
+        });
     }
 
+
+    private void handleSubmit(){
+        String searchText = _editText.getText().toString();
+        if(validSubmission(searchText)){
+            CharSequence cs = "Seaching for: "+ searchText;
+            Toast.makeText(getApplicationContext(), cs  , Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    private boolean validSubmission(String searchText) {
+        //TODO - insure searchText is of the form City, State, Occupation
+        String value = searchText.replace(',', ' ');
+        return true;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
