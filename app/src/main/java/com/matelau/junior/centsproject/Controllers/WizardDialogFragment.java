d54b3b9c69ecec6943827e9431c64e63d9a83f96
Junior Matelau <matelau@gmail.com>
2015-01-18 22:23:31 -0700

    wizard stubbed out with text ui and animations

--- app/src/main/java/com/matelau/junior/centsproject/Controllers/WizardDialogFragment.java
+++ app/src/main/java/com/matelau/junior/centsproject/Controllers/WizardDialogFragment.java
@@ -1,12 +1,15 @@
 package com.matelau.junior.centsproject.Controllers;
 
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -18,37 +21,111 @@ import com.matelau.junior.centsproject.R;
  * A simple {@link Fragment} subclass.
  */
 public class WizardDialogFragment extends DialogFragment {
+    private String LOG_TAG = WizardDialogFragment.class.getSimpleName();
     LinearLayout _rootLayout;
     TextView _question;
     ListView _answers;
     ArrayAdapter<String> _answersAdapter;
+    String[] _college_answers = new String[] {"I'm not sure where I want to study", "I'm not sure what I want to study","I'm not sure about either"};
+    String[] _cost_answers = new String[] {"I'm curious about the cost of living in a city", "I'm curious about my spending breakdown"};
 
 
     public WizardDialogFragment() {
         // Required empty public constructor
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        _rootLayout = (LinearLayout) inflater.inflate(R.layout.fragment_wizard_dialog, container, false);
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        _rootLayout = (LinearLayout) inflater.inflate(R.layout.fragment_wizard_dialog, null, false);
         //get question text
         _question = (TextView) _rootLayout.findViewById(R.id.wizard_question);
         _question.setText("What Best Describes Your Situation?");
         //get question list
-        String[] initial_answers = new String[]{"Starting or Returning to College", "Graduating College or Changing Jobs", "Moving to a New City", "Just Looking Around"};
+        String[] initial_answers = new String[]{"Starting or Returning to College", "Graduating College or Changing Jobs", "Considering Moving or Financial Spending", "Just Looking Around"};
 
         _answers = (ListView) _rootLayout.findViewById(R.id.wizard_options);
+        _answers.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectInitialItem(position);
+            }
+        });
         _answersAdapter = new ArrayAdapter<String>(getActivity(), R.layout.wizard_item, initial_answers);
         _answers.setAdapter(_answersAdapter);
 
+        builder.setView(_rootLayout);
 
-
-
-
-        return _rootLayout;
+        return builder.create();
     }
 
+    private void selectInitialItem(int pos){
+        Log.d(LOG_TAG, "Initial Item Selected: " + pos);
+        switch(pos) {
+            case 0:
+                //College or Major inquiry
+                _answersAdapter = new ArrayAdapter<String>(getActivity(), R.layout.wizard_item, _college_answers);
+                _answers.setAdapter(_answersAdapter);
+                _answers.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        selectCollegeOrMajor(position);
+                    }
+                });
+                break;
+            case 1:
+                //Career inquiry
+                //TODO launch career comparison fragment
+                break;
+            case 2:
+                //Cost of Living or spending breakdown inquiry
+                _answersAdapter = new ArrayAdapter<String>(getActivity(), R.layout.wizard_item, _cost_answers);
+                _answers.setAdapter(_answersAdapter);
+                _answers.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        selectCost(position);
+                    }
+                });
+                //TODO launch living inquiry
+                break;
+            default:
+                //Examples?
+                break;
+
+        }
+    }
+
+
+    private void selectCollegeOrMajor(int pos){
+        Log.d(LOG_TAG, "College Item Selected: " + pos);
+        switch(pos){
+            case 0:
+                //College Inquiry
+                break;
+            case 1:
+                //Major Inquiry
+                break;
+            default:
+                //still unsure
+                break;
+        }
+    }
+
+    private void selectCost(int pos){
+        Log.d(LOG_TAG, "Cost Item Selected: " + pos);
+        switch(pos) {
+            case 0:
+                //Cost of Living
+                break;
+            case 1:
+                //Spending
+                break;
+            default:
+                break;
+        }
+
+    }
 
 }
